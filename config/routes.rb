8e23c1e3ffa23df5d435bbe56e3163cbf8cee8fa
1noseA <1noseA2L@gmail.com>
2020-04-05 17:22:53 +0900

    [Add]routesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users,only: [:index,:show,:edit,:update] do
+    resource :items
+    resource :coordinates
+  end
 
+  #resources :categories
+  #resources :scenes
+  #resources :seasons
+  
   root 'homes#top'
-  get '/about' => 'homes#about'
+  get '/about' => 'homes#about' 
 end
