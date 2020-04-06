8649794dd04e3ddf5486a9fcd6bdd57ef1e1d68e
1noseA <1noseA2L@gmail.com>
2020-04-06 17:24:02 +0900

    [Add]itemコントローラーを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -10,15 +12,32 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+    redirect_to items_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+    redirect_to items_path
+    else
+    render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_image,:name,:text)
   
 end
