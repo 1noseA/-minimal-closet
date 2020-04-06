caf6ae7b59c5d25d77d1a8cca8ed64c2ddfac890
1noseA <1noseA2L@gmail.com>
2020-04-06 14:35:20 +0900

    [Add]ヘッダー・フッターを追加、Bootstrap4導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users,only: [:index,:show,:edit,:update] do
-    resource :items
-    resource :coordinates
-  end
+  resources :users,only: [:index,:show,:edit,:update]
+  resources :items
+  resources :coordinates
+  resources :calendars
 
   #resources :categories
   #resources :scenes
   #resources :seasons
-  
+
   root 'homes#top'
   get '/about' => 'homes#about' 
 end
