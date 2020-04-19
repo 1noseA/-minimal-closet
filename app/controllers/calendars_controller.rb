bc6953974f839427a365458366476a3bbe60dc60
1noseA <1noseA2L@gmail.com>
2020-04-19 15:58:23 +0900

    [Add]calendarコントローラーとビューを追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,2 +1,52 @@
 class CalendarsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @calendars = @user.calendars
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @calendar = Calendar.new
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @calendar = Calendar.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    if Calendar.create(calendar_params)
+      redirect_to user_calendars_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @calendar = Calendar.find(params[:id])
+    @calendar.destroy
+    redirect_to user_calendars_path(@user), notice:"削除しました"
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @calendar = Calendar.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @calendar = Calendar.find(params[:id])
+    if @calendar.update(calendar_params)
+      redirect_to user_calendars_path(@user), notice: "編集しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def calendar_params
+    params.require(:calendar).permit(:user_id, :day_image, :text, :start_time)
+  end
 end
