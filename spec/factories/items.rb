7a7d38dbf16890ce4595b712a6d54ea9fc7af036
1noseA <1noseA2L@gmail.com>
2020-05-13 11:59:54 +0900

    [Fix]失敗していたテストを修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,6 +2,9 @@ FactoryBot.define do
   factory :item do
     name { 'aaa' }
     text { 'aaa' }
+    category_id { '1' }
+    scene_id { '1' }
+    season_id { '1' }
     item_image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))}
     user
   end
